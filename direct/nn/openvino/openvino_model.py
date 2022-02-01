b6b3942a7f2bc0a64f0529c8dd1808771858fbb5
Anna Likholat <anna.likholat@intel.com>
2022-02-01 17:20:00 +0300

    layer replacer

--- direct/nn/openvino/openvino_model.py
+++ direct/nn/openvino/openvino_model.py
@@ -6,6 +6,45 @@ import torch.nn as nn
 import io
 
 
+class InstanceNorm2dFunc(torch.autograd.Function):
+    @staticmethod
+    def symbolic(g, cls, input):
+        c_scale = g.op("Constant", value_t=cls.scale_one)
+        c_bias = g.op("Constant", value_t=cls.bias_zero)
+        return g.op("InstanceNormalization", input, c_scale, c_bias)
+
+    @staticmethod
+    def forward(self, cls, input):
+        y = cls.origin_forward(input)
+        return y
+
+
+class InstanceNorm2dONNX(nn.InstanceNorm2d):
+    """
+    This is a support class which helps export network with InstanceNorm2d in ONNX format.
+    """
+
+    def __init__(self, num_features):
+        super().__init__(num_features)
+        self.origin_forward = super().forward
+        self.scale_one = torch.ones(num_features)
+        self.bias_zero = torch.zeros(num_features)
+
+    def forward(self, input):
+        y = InstanceNorm2dFunc.apply(self, input).clone()
+        return y
+
+
+def convert_layer(model):
+    for name, l in model.named_children():
+        layer_type = l.__class__.__name__
+        if layer_type == "InstanceNorm2d":
+            new_layer = InstanceNorm2dONNX(l.num_features)
+            setattr(model, name, new_layer)
+        else:
+            convert_layer(l)
+
+
 class OpenVINOModel(nn.Module):
     def __init__(self, model):
         super().__init__()
@@ -30,6 +69,8 @@ class OpenVINOModel(nn.Module):
             self.input = [kwargs[k] for k in args]
         else:
             raise ValueError(f"The model is not supported by OpenVINO: {self.model.__class__}")
+
+        convert_layer(self.model)
 
         with torch.no_grad():
             buf = io.BytesIO()
--- direct/nn/unet/unet_2d.py
+++ direct/nn/unet/unet_2d.py
@@ -12,35 +12,6 @@ from torch.nn import functional as F
 from direct.data import transforms as T
 
 
-class InstanceNorm2dFunc(torch.autograd.Function):
-    @staticmethod
-    def symbolic(g, cls, input):
-        c_scale = g.op("Constant", value_t=cls.scale_one)
-        c_bias = g.op("Constant", value_t=cls.bias_zero)
-        return g.op("InstanceNormalization", input, c_scale, c_bias)
-
-    @staticmethod
-    def forward(self, cls, input):
-        y = cls.origin_forward(input)
-        return y
-
-
-class InstanceNorm2dONNX(nn.InstanceNorm2d):
-    """
-    This is a support class which helps export network with InstanceNorm2d in ONNX format.
-    """
-
-    def __init__(self, num_features):
-        super().__init__(num_features)
-        self.origin_forward = super().forward
-        self.scale_one = torch.ones(num_features)
-        self.bias_zero = torch.zeros(num_features)
-
-    def forward(self, input):
-        y = InstanceNorm2dFunc.apply(self, input).clone()
-        return y
-
-
 class ConvBlock(nn.Module):
     """
     A Convolutional Block that consists of two convolution layers each followed by
@@ -67,11 +38,11 @@ class ConvBlock(nn.Module):
 
         self.layers = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            InstanceNorm2dONNX(out_channels),
+            nn.InstanceNorm2d(out_channels),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
             nn.Dropout2d(dropout_probability),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            InstanceNorm2dONNX(out_channels),
+            nn.InstanceNorm2d(out_channels),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
             nn.Dropout2d(dropout_probability),
         )
@@ -118,7 +89,7 @@ class TransposeConvBlock(nn.Module):
 
         self.layers = nn.Sequential(
             nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2, bias=False),
-            InstanceNorm2dONNX(out_channels),
+            nn.InstanceNorm2d(out_channels),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
         )
 
